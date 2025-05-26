486dde8ed97baf587ebb4df11ac4ba328ff7b18d
gui231208 <231208gui@gmail.com>
2025-05-26 08:54:51 -0300

    Adicionar arquivos de projeto.

---
+++ WebApiFinalEtecPrimeiroSemestre/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApiFinalEtecPrimeiroSemestre.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebApiFinalEtecPrimeiroSemestreContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebApiFinalEtecPrimeiroSemestreContext") ?? throw new InvalidOperationException("Connection string 'WebApiFinalEtecPrimeiroSemestreContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
