eb4f4d65acad8852810d4bde4ddb87a375cfa402
gui231208 <231208gui@gmail.com>
2025-06-09 09:09:45 -0300

    cors

--- WebApiFinalEtecPrimeiroSemestre/Models/Produtos.cs
+++ WebApiFinalEtecPrimeiroSemestre/Models/Produtos.cs
@@ -6,5 +6,22 @@
         public string? Descricao{ get; set; }
         public int Quantidade{ get; set; }
         public decimal Valor{ get; set; }
+
+
+
+
+        //tipo de dados 
+
+        //string nome = "Etec"; //Tipo de referência
+        //char letra = 'E';
+
+        //int valor = 10; // Números inteiros
+        //long valor1 = 10L; // Números inteiros
+        //byte valorB = 127; //Números inteiros
+        
+        //float valorF = 10.5F; //Números reais
+        //double valorD = 10.5; //Números reais
+
     }
 }
+
--- WebApiFinalEtecPrimeiroSemestre/Program.cs
+++ WebApiFinalEtecPrimeiroSemestre/Program.cs
@@ -1,17 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using WebApiFinalEtecPrimeiroSemestre.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<WebApiFinalEtecPrimeiroSemestreContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("WebApiFinalEtecPrimeiroSemestreContext") ?? throw new InvalidOperationException("Connection string 'WebApiFinalEtecPrimeiroSemestreContext' not found.")));
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+
+// Políticas de Cors Origin
+
+builder.Services.AddCors(etecPoliticas =>
+{
+    etecPoliticas.AddPolicy("EtecAll",
+        builder =>
+        {
+            builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+});
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors("EtecAll");
 
 app.UseHttpsRedirection();
 
